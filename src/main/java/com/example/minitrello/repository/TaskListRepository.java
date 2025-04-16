cc7b9c99d85cc4f757d26ad749d5b09d0fe6d884
Mohamed atef <89284188+hngara@users.noreply.github.com>
2025-04-16 09:59:24 +0200

    Set up Repository Interfaces

---
+++ src/main/java/com/example/minitrello/repository/TaskListRepository.java
@@ -0,0 +1,9 @@
+package com.example.minitrello.repository;
+
+import com.example.minitrello.model.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, Long> {
+}
